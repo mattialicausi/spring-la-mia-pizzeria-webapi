e785614fe0451bbc9bb43c5d484be694bc8af10c
mattialicausi <licausivitomattia@gmail.com>
2023-03-29 15:03:03 +0200

    aggiunto insert in import.sql per aggiungere elementi nel db

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza napoletana.', 'Margherita', 9.50);
+INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza piccante', 'Diavola', 12.00);
+INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza gustosa', 'Marinara', 10.00);
+INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('La nostra specialità', 'Napoli', 13.00);
+INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza golosa', '4 formaggi', 13.00);
