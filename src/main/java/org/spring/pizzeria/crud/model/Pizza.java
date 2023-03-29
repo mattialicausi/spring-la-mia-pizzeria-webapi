6915a05bad231626931643de66a89c1d209e6682
mattialicausi <licausivitomattia@gmail.com>
2023-03-29 16:32:31 +0200

    aggiunto campo image in Model Pizza, modificato import.sql e aggiunto alla view la foto

--- src/main/java/org/spring/pizzeria/crud/model/Pizza.java
+++ src/main/java/org/spring/pizzeria/crud/model/Pizza.java
@@ -23,6 +23,7 @@ public class Pizza {
     private String description;
 
     private BigDecimal price;
+    private String image;
 
 
     //getter e setter di tutti i campi
@@ -58,5 +59,13 @@ public class Pizza {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza napoletana.', 'Margherita', 9.50);
-INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza piccante', 'Diavola', 12.00);
-INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza gustosa', 'Marinara', 10.00);
-INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('La nostra specialità', 'Napoli', 13.00);
-INSERT INTO db_pizzeria.pizze (description, name, price) VALUES('Pizza golosa', '4 formaggi', 13.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza napoletana.', 'https://c4.wallpaperflare.com/wallpaper/517/845/145/pizza-italian-food-vegetarian-food-pizza-margherita-wallpaper-preview.jpg', 'Margherita', 9.50);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza piccante', 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/03/d2.jpg', 'Diavola', 12.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza gustosa', 'https://media-assets.lacucinaitaliana.it/photos/61fac5f28f675ad335f0dc34/16:9/w_2560%2Cc_limit/Marasco-Marinara-ai-quattro-pomodori-c.jpg', 'Marinara', 10.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('La nostra specialità', 'https://staticfanpage.akamaized.net/wp-content/uploads/sites/4/2018/07/pizza-vincenzo-capuano-social.jpg', 'Napoli', 13.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza golosa', 'https://www.lucianopignataro.it/wp-content/uploads/2019/06/Pizza-Quattro-formaggi.jpg' '4 formaggi', 13.00);
+
+
