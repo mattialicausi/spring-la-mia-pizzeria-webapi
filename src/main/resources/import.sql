4cbf5c658627926e2f332aa3c37ee1d5c6c19d67
mattialicausi <licausivitomattia@gmail.com>
2023-03-30 16:31:32 +0200

    aggiunto img logo in navbar e aggiunto euro in prezzo in show

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,7 @@ INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza na
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza piccante', 'https://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/03/d2.jpg', 'Diavola', 12.00);
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza gustosa', 'https://media-assets.lacucinaitaliana.it/photos/61fac5f28f675ad335f0dc34/16:9/w_2560%2Cc_limit/Marasco-Marinara-ai-quattro-pomodori-c.jpg', 'Marinara', 10.00);
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('La nostra specialità', 'https://staticfanpage.akamaized.net/wp-content/uploads/sites/4/2018/07/pizza-vincenzo-capuano-social.jpg', 'Napoli', 13.00);
-INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza golosa', 'https://www.lucianopignataro.it/wp-content/uploads/2019/06/Pizza-Quattro-formaggi.jpg' '4 formaggi', 13.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza golosa', 'https://www.lucianopignataro.it/wp-content/uploads/2019/06/Pizza-Quattro-formaggi.jpg', 'Quattro formaggi', 13.00);
+INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza molto buona', 'https://media-cdn.tripadvisor.com/media/photo-s/10/04/b0/77/pizza-kebab-completa.jpg', 'Kebab', 15.00);
 
 
