7b0ca66a2105bddb4e47ebe00e87e08891b50f4d
mattialicausi <licausivitomattia@gmail.com>
2023-04-07 15:37:18 +0200

    aggiunto file SecurityConfiguration per gestire gli accessi e creato utenti su import.sql

---
+++ src/main/java/org/spring/pizzeria/crud/security/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package org.spring.pizzeria.crud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    //impprto un metodo che restituisce un nuovo DTUserService
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    //metodo che restiruisce un oggetto per criptare e decrittare le password
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    //metodo che mette insieme UserDetailsService e passwordEncoder per eseguire le query per autenticare gli utenti
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        // creo un authentication provider basato su database (DAO)
+        provider.setUserDetailsService(userDetailsService());
+
+        // associo il mio DatabaseUserDetailsService
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    /*
+    * home "/" USER, ADMIN
+    * pizza "/pizza" USER, ADMIN
+    * edit "/pizza/edit/{id}" ADMIN *****************************
+    * create "/pizza/create/{id}" ADMIN *************************
+    * delete "/pizza/delete/{id} ADMIN **************************
+    * show "pizza/{id} USER, ADMIN
+    * offer "/offer/...." ADMIN *********************************
+    * ingredient "/ingredient/..." ADMIN ************************
+    * */
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/ingredient", "/ingredient/**").hasAuthority("ADMIN")
+                .requestMatchers("/offer/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizza/edit/**", "/pizza/create", "/pizza/delete/**").hasAuthority("ADMIN")
+                .requestMatchers("/pizza", "/pizza/**", "/").hasAnyAuthority("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/pizza/**").hasAuthority("ADMIN")
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout()
+                .and().exceptionHandling();
+        return http.build();
+    }
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,16 +4,19 @@ INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza gu
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('La nostra specialità', 'https://staticfanpage.akamaized.net/wp-content/uploads/sites/4/2018/07/pizza-vincenzo-capuano-social.jpg', 'Napoli', 13.00);
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza golosa', 'https://www.lucianopignataro.it/wp-content/uploads/2019/06/Pizza-Quattro-formaggi.jpg', 'Quattro formaggi', 13.00);
 INSERT INTO db_pizzeria.pizze (description, image, name, price) VALUES('Pizza molto buona', 'https://media-cdn.tripadvisor.com/media/photo-s/10/04/b0/77/pizza-kebab-completa.jpg', 'Kebab', 15.00);
+
 INSERT INTO db_pizzeria.offer (end_date, start_date, title, pizza_id) VALUES('2023-12-09', '2023-6-6', 'Paghi una, prendi due',1);
 INSERT INTO db_pizzeria.offer (end_date, start_date, title, pizza_id) VALUES('2023-12-09', '2023-6-6', 'Paghi una, prendi due',2);
 INSERT INTO db_pizzeria.offer (end_date, start_date, title, pizza_id) VALUES('2023-12-09', '2023-6-6', 'Paghi una, prendi due',3);
 INSERT INTO db_pizzeria.offer (end_date, start_date, title, pizza_id) VALUES('2023-12-09', '2023-6-6', 'Paghi una, prendi due',4);
 INSERT INTO db_pizzeria.offer (end_date, start_date, title, pizza_id) VALUES('2023-12-09', '2023-6-6', 'Paghi una, prendi due',5);
+
 INSERT INTO db_pizzeria.ingredients (name) VALUES('Mozzarella');
 INSERT INTO db_pizzeria.ingredients (name) VALUES('Salsa al pomodoro');
 INSERT INTO db_pizzeria.ingredients (name) VALUES('Basilico');
 INSERT INTO db_pizzeria.ingredients (name) VALUES('Salame');
 INSERT INTO db_pizzeria.ingredients (name) VALUES('Olive');
+
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(1, 1);
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(1,2);
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(1, 3);
@@ -22,5 +25,16 @@ INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(2, 1);
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(2,2);
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(2, 4);
 INSERT INTO db_pizzeria.ingredient_pizza (pizza_id, ingredient_id) VALUES(2,5);
+
+INSERT INTO db_pizzeria.`user` (email, first_name, last_name, password) VALUES('mattia@gmail.com', 'Mattia', 'Rossi', '{noop}mattia');
+INSERT INTO db_pizzeria.`user` (email, first_name, last_name, password) VALUES('valeria@gmail.com', 'Valeria', 'Rossi', '{noop}valeria');
+
+INSERT INTO db_pizzeria.`role` (id, name) VALUES(1, 'ADMIN');
+INSERT INTO db_pizzeria.`role` (id, name) VALUES(2, 'USER');
+
+INSERT INTO db_pizzeria.user_roles (user_id, roles_id) VALUES(1, 1);
+INSERT INTO db_pizzeria.user_roles (user_id, roles_id) VALUES(2, 2);
+
+
 
 
