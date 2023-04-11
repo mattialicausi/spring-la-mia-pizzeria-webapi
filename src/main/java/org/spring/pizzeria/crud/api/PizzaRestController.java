5c6329a9519b05ccf94e6b42e98d00b13758aac7
mattialicausi <licausivitomattia@gmail.com>
2023-04-11 14:47:55 +0200

    aggiunto api per visualizzare la pizza singola

--- src/main/java/org/spring/pizzeria/crud/api/PizzaRestController.java
+++ src/main/java/org/spring/pizzeria/crud/api/PizzaRestController.java
@@ -1,9 +1,12 @@
 package org.spring.pizzeria.crud.api;
 
+import org.spring.pizzeria.crud.exceptions.PizzaNotFoundException;
 import org.spring.pizzeria.crud.model.Pizza;
 import org.spring.pizzeria.crud.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,4 +30,18 @@ public class PizzaRestController {
         return pizzaService.getAllPizze();
 
     }
+
+    //pizza singola
+    @GetMapping("/{id}")
+    public Pizza getById(@PathVariable Integer id) {
+
+        try {
+            return pizzaService.getById(id);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
 }
